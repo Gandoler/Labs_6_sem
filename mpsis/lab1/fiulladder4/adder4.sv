239e6b9b34bd8b466e878f17c147505c964a6604
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-03-02 10:33:08 +0300

    Update adder4.sv

--- mpsis/lab1/fiulladder4/adder4.sv
+++ mpsis/lab1/fiulladder4/adder4.sv
@@ -1,15 +1,3 @@
-module fulladder(
-    input logic a_i,
-    input logic b_i,
-    input logic carry_i,  
-    output logic sum_o, 
-    output logic carry_o
-);
-assign sum_o = (a_i ^ b_i) ^ carry_i;
-assign carry_o = (a_i & b_i) | ((a_i ^ b_i) & carry_i);
-endmodule
-
-
 module adder4(
     input logic [3:0] a_i,
     input logic [3:0] b_i,
